a9eae7cd1805dc94e448667ddc9194c615ae4594
eried <1091420+eried@users.noreply.github.com>
2022-09-09 14:06:39 +0200

    Icon and improved functionality

---
+++ COMPortNotifier/COMPortNotifier/FormMain.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace COMPortNotifier
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.serialPortMonitor = new System.IO.Ports.SerialPort(this.components);
+            this.listBoxCOMPorts = new System.Windows.Forms.ListBox();
+            this.timerMonitor = new System.Windows.Forms.Timer(this.components);
+            this.notifyIconNotification = new System.Windows.Forms.NotifyIcon(this.components);
+            this.labelWaiting = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBoxCOMPorts
+            // 
+            this.listBoxCOMPorts.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxCOMPorts.FormattingEnabled = true;
+            this.listBoxCOMPorts.IntegralHeight = false;
+            this.listBoxCOMPorts.ItemHeight = 28;
+            this.listBoxCOMPorts.Location = new System.Drawing.Point(0, 0);
+            this.listBoxCOMPorts.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.listBoxCOMPorts.Name = "listBoxCOMPorts";
+            this.listBoxCOMPorts.Size = new System.Drawing.Size(416, 212);
+            this.listBoxCOMPorts.TabIndex = 0;
+            // 
+            // timerMonitor
+            // 
+            this.timerMonitor.Interval = 1000;
+            this.timerMonitor.Tick += new System.EventHandler(this.timerMonitor_Tick);
+            // 
+            // notifyIconNotification
+            // 
+            this.notifyIconNotification.Text = "notifyIcon1";
+            this.notifyIconNotification.Visible = true;
+            // 
+            // labelWaiting
+            // 
+            this.labelWaiting.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelWaiting.Location = new System.Drawing.Point(0, 0);
+            this.labelWaiting.Name = "labelWaiting";
+            this.labelWaiting.Size = new System.Drawing.Size(416, 212);
+            this.labelWaiting.TabIndex = 1;
+            this.labelWaiting.Text = "Waiting for Serial Ports...";
+            this.labelWaiting.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 212);
+            this.Controls.Add(this.labelWaiting);
+            this.Controls.Add(this.listBoxCOMPorts);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.Name = "FormMain";
+            this.Text = "Connected COM Ports";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPortMonitor;
+        private System.Windows.Forms.ListBox listBoxCOMPorts;
+        private System.Windows.Forms.Timer timerMonitor;
+        private System.Windows.Forms.NotifyIcon notifyIconNotification;
+        private System.Windows.Forms.Label labelWaiting;
+    }
+}
+
---
+++ COMPortNotifier/COMPortNotifier/FormMain.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMPortNotifier
+{
+    public partial class FormMain : Form
+    {
+        private Dictionary<string,DetectedSerialPort> _ports;
+
+        public FormMain()
+        {
+            InitializeComponent();
+            _ports = new Dictionary<string, DetectedSerialPort>();
+            timerMonitor.Enabled = true;
+        }
+
+        private void timerMonitor_Tick(object sender, EventArgs e)
+        {
+            /*foreach (var p in _ports)
+                p.Value.Connected = false;*/
+
+            var updateList = false;
+            var s = SerialPort.GetPortNames();
+
+            foreach (var p in _ports) // Check for the disconnected ones
+                if (!s.Contains(p.Key))
+                    if (p.Value.Connected)
+                    {
+                        p.Value.Connected = false;
+                        updateList = true;
+                    }
+
+            foreach (var p in s) // Check for new ports to add
+            {
+                if (!_ports.ContainsKey(p))
+                {
+                    _ports.Add(p, new DetectedSerialPort());
+                    updateList = true;
+                }
+                else
+                {
+                    if (!_ports[p].Connected)
+                    {
+                        _ports[p].Timestamp = DateTime.Now;
+                        _ports[p].Connected = true;
+                        updateList = true;
+                    }
+                }
+            }
+
+            //if (updateList)
+            var selected = listBoxCOMPorts.SelectedIndex;
+            labelWaiting.Visible = false;
+            listBoxCOMPorts.Visible = true;
+            listBoxCOMPorts.Items.Clear();
+
+            foreach (var p in _ports)
+                listBoxCOMPorts.Items.Insert(0, DetectedSerialPort.ToString(p));
+            listBoxCOMPorts.SelectedIndex = selected == -1? 0: selected;
+        }
+    }
+
+    internal class DetectedSerialPort
+    {
+        public DateTime Timestamp { get; set; }
+        public bool Connected { get; set; }
+
+        public DetectedSerialPort()
+        {
+            this.Timestamp = DateTime.Now;
+            this.Connected = true;
+        }
+
+        public new String ToString()
+        {
+            return "\tConnected: " + (this.Connected ? "YES" : "NO") + "\tLast activity: " + DetectedSerialPort.FriendlyTimestamp(Timestamp);
+        }
+
+        private static string FriendlyTimestamp(DateTime timestamp)
+        {
+            var t = DateTime.Now.Subtract(timestamp);
+
+            if (t.TotalMinutes >= 60)
+                return timestamp.ToString();
+            else
+                if (t.TotalMinutes < 1)
+            {
+                return t.TotalSeconds > 5 ? "less than a minute ago": "just now";
+            }
+            else
+                return Math.Floor(t.TotalMinutes) + " minutes ago";
+        }
+
+        internal static object ToString(KeyValuePair<string, DetectedSerialPort> p)
+        {
+            return p.Key + p.Value.ToString();
+        }
+    }
+}
